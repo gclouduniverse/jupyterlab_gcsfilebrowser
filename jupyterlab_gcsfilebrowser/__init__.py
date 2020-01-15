f1017966d68dd5095511fcaeb4ce7d70bfcf4303
Curtis Wilkes <cbwilkes@google.com>
2020-01-15 17:21:54 -0500

    Move __init__py to __init__.py
    Change-Id: I325eb071efad230c619578aca0ea8c81a81ec307

---
+++ jupyterlab_gcsfilebrowser/__init__.py
@@ -0,0 +1,25 @@
+from notebook.utils import url_path_join
+
+from jupyterlab_gcsfilebrowser.handlers import GCSHandler
+from jupyterlab_gcsfilebrowser.version import VERSION
+
+__version__ = VERSION
+
+def _jupyter_server_extension_paths():
+    return [{
+        'module': 'jupyterlab_gcsfilebrowser'
+    }]
+
+def load_jupyter_server_extension(nb_server_app):
+    """
+    Called when the extension is loaded.
+    Args:
+        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
+    """
+    host_pattern = '.*$'
+    app = nb_server_app.web_app
+    gcp_v1_endpoint = url_path_join(app.settings['base_url'], 'gcp', 'v1')
+    app.add_handlers(host_pattern, [
+      (url_path_join(gcp_v1_endpoint, 'gcs', 'auth'), AuthHandler)
+      (url_path_join(gcp_v1_endpoint, 'gcs'), GCSHandler)
+    ])
