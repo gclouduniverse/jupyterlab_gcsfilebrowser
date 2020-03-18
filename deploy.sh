3b41a2358ebf4c67f79768aef1f38abcc2aacc12
Jason Prodonovich <prodonjs@google.com>
2020-03-18 15:59:36 -0400

    Update deploy.sh with correct install command

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ ARCHIVE="$(ls dist)"
 
 gcloud compute scp "dist/${ARCHIVE?}" "jupyter@${INSTANCE_NAME?}:/home/jupyter"
 gcloud compute ssh "jupyter@${INSTANCE_NAME?}" -- \
-       "sudo pip3 install ${ARCHIVE?} && \
+       "sudo su -p -l root -c \"/opt/conda/bin/pip install ${ARCHIVE}\" && \
         sudo su -p -l root -c '/opt/conda/bin/jupyter lab build' && \
         sudo service jupyter restart"
 
